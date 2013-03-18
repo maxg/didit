15a81b058c02db9ccf07715d91fb0c20f529bb4c
Max Goldman <maxg@mit.edu>
2013-03-18 19:53:27 -0400

    Factor out function to obtain student commit log

--- git.js
+++ git.js
@@ -5,6 +5,10 @@ var spawn = require('child_process').spawn;
 var config = require('./config');
 var log = require('./logger').cat('git');
 
+function studentSourcePath(spec) {
+  return [ config.student.repos, config.student.semester, spec.kind, spec.proj, spec.users.join('-') ].join('/') + '.git';
+}
+
 // spawn a process and log stderr
 // options must include a 'pipe' setting for stderr
 function spawnAndLog(command, args, options) {
@@ -13,6 +17,29 @@ function spawnAndLog(command, args, options) {
     log.error({ err: line, command: command, args: args, options: options });
   });
   return child;
+}
+
+// obtain commit log for student source
+// callback returns an array of commit info objects
+exports.studentSourceLog = function(spec, range, callback) {
+  log.info({ spec: spec, range: range}, 'studentSourceLog');
+  var names = [ 'rev', 'author', 'authoremail', 'authortime', 'committer', 'committeremail', 'committertime', 'subject' ];
+  var format = [ '%h', '%an', '%ae', '%at', '%cn', '%ce', '%ct', '%s' ].join('%x00')
+  var lines = [];
+  var out = byline(spawnAndLog('git', [ 'log',
+    '--pretty=format:' + format
+  ].concat(range).concat([ '--' ]), {
+    cwd: studentSourcePath(spec),
+    stdio: 'pipe'
+  }).stdout);
+  out.on('data', function(line) {
+    var res = {};
+    line.split('\0').forEach(function(val, idx) { res[names[idx]] = val; });
+    lines.push(res);
+  });
+  out.on('end', function() {
+    callback(null, lines);
+  });
 }
 
 // clone student source
@@ -26,7 +53,7 @@ exports.cloneStudentSource = function(spec, dest, callback) {
       log.info('cloneStudentSource', 'clone');
       spawnAndLog('git', [ 'clone',
         '--quiet', '--no-checkout', '--depth', '10', '--branch', 'master',
-        [ 'file:/', config.student.repos, config.student.semester, spec.kind, spec.proj, spec.users.join('-') ].join('/') + '.git',
+        'file://' + studentSourcePath(spec),
         '.'
       ], {
         cwd: dest,
@@ -52,22 +79,8 @@ exports.cloneStudentSource = function(spec, dest, callback) {
     // obtain student revision metadata
     log: [ 'checkout', function(next) {
       log.info('cloneStudentSource', 'log');
-      var names = [ 'rev', 'author', 'authoremail', 'authortime', 'committer', 'committeremail', 'committertime', 'subject' ];
-      var format = [ '%h', '%an', '%ae', '%at', '%cn', '%ce', '%ct', '%s' ].join('%x00')
-      var lines = [];
-      var out = byline(spawn('git', [ 'log',
-        '-1', '--pretty=format:' + format
-      ], {
-        cwd: dest,
-        stdio: 'pipe'
-      }).stdout);
-      out.on('data', function(line) {
-        var res = {};
-        line.split('\0').forEach(function(val, idx) { res[names[idx]] = val; });
-        lines.push(res);
-      });
-      out.on('end', function() {
-        next(null, lines[0]);
+      exports.studentSourceLog(spec, [ '-1', spec.rev ], function(err, lines) {
+        next(err, lines[0]);
       });
     } ]
   }, function(err, results) {
@@ -132,3 +145,19 @@ exports.fetchBuilder = function(spec, dest, callback) {
     callback(err, results ? results.archive.substring(0, 7) : null);
   });
 };
+
+// command-line git
+if (require.main === module) {
+  var args = process.argv.slice(2);
+  if (args.join(' ').match(/^log \w+ \w+ [\w-]+/)) {
+    log.info('git', args[0]);
+    exports.studentSourceLog({
+      kind: args[1], proj: args[2], users: args[3].split('-')
+    }, args.slice(4), function(err, lines) {
+      if (err) { log.error(err, 'error'); }
+      log.info(lines, 'result');
+    });
+  } else {
+    log.error('unknown command', args);
+  }
+}
