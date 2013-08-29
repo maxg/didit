2ac1af23323784c7cd413676762e53560d9e5d56
Max Goldman <maxg@mit.edu>
2013-08-29 17:10:49 -0400

    Additional tests for Git

--- test/git.js
+++ test/git.js
@@ -1,10 +1,19 @@
 var fixtures = require('./fixtures');
+var moment = require('moment');
+var should = require('should');
 
 describe('git', function() {
   
   var git = require('../git');
   
   var fix = fixtures();
+  var repos = [
+    { kind: 'labs', proj: 'lab1', users: [ 'alice' ] },
+    { kind: 'labs', proj: 'lab1', users: [ 'bob' ] },
+    { kind: 'labs', proj: 'lab2', users: [ 'alice' ] },
+    { kind: 'projects', proj: 'helloworld', users: [ 'alice', 'bob' ] }
+  ];
+  var repo = repos[0];
   
   before(function(done) {
     fix.files(this.test, done);
@@ -18,9 +27,67 @@ describe('git', function() {
     fix.forget();
   });
   
+  describe('findStudentRepos', function() {
+    it('should return repo specifications', function(done) {
+      git.findStudentRepos({}, function(err, found) {
+        found.should.eql(repos);
+        done(err);
+      });
+    });
+    it('kind restriction should limit repos', function(done) {
+      git.findStudentRepos({ kind: 'labs' }, function(err, found) {
+        found.should.eql(repos.slice(0, 3));
+        done(err);
+      });
+    });
+    it('proj restriction should limit repos', function(done) {
+      git.findStudentRepos({ proj: 'helloworld' }, function(err, found) {
+        found.should.eql([ repos[3] ]);
+        done(err);
+      });
+    });
+    it('user restriction should limit repos', function(done) {
+      git.findStudentRepos({ kind: 'labs', users: [ 'bob' ]}, function(err, found) {
+        found.should.eql([ repos[1] ]);
+        done(err);
+      });
+    });
+  });
+  
+  describe('studentSourceRev', function() {
+    it('should return current revision', function(done) {
+      git.studentSourceRev(repo, function(err, rev) {
+        rev.should.equal('236e692');
+        done(err);
+      });
+    });
+  });
+  
+  describe('studentSourceRevAt', function() {
+    it('should return current revision', function(done) {
+      git.studentSourceRevAt(repo, moment(), function(err, rev) {
+        rev.should.equal('236e692');
+        done(err);
+      });
+    });
+    it('should return past revision', function(done) {
+      git.studentSourceRevAt(repo, moment('2013-08-09T20:09:30 Z'), function(err, rev) {
+        rev.should.equal('cb138ec');
+        done(err);
+      });
+    });
+    it('should fail for prehistoric time', function(done) {
+      git.studentSourceRevAt(repo, moment('2013-08-09T10:00:00 Z'), function(err, rev) {
+        should.exist(err);
+        should.not.exist(rev);
+        done();
+      });
+    });
+  });
+  
   describe('studentSourceLog', function() {
     it('should return a full commit log', function(done) {
-      git.studentSourceLog({ kind: 'labs', proj: 'lab1', users: [ 'alice' ]}, [], function(err, lines) {
+      git.studentSourceLog(repo, [], function(err, lines) {
         lines.map(function(line) {
           return { rev: line.rev, msg: line.subject };
         }).should.eql([
@@ -31,7 +98,7 @@ describe('git', function() {
       });
     });
     it('should return a single commit log', function(done) {
-      git.studentSourceLog({ kind: 'labs', proj: 'lab1', users: [ 'alice' ]}, [ '-1' ], function(err, lines) {
+      git.studentSourceLog(repo, [ '-1' ], function(err, lines) {
         lines.map(function(line) {
           return { rev: line.rev, msg: line.subject };
         }).should.eql([
@@ -63,6 +130,15 @@ describe('git', function() {
         })
       });
     })
+  });
+  
+  describe('builderRev', function() {
+    it('should return current revision', function(done) {
+      git.builderRev(function(err, staffrev) {
+        staffrev.should.equal('1178dcf');
+        done(err);
+      });
+    });
   });
   
   describe('fetchBuilder', function() {
