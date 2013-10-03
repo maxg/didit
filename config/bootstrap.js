91f6ee530326dd40ef0f44932361d6026242d07d
Max Goldman <maxg@mit.edu>
2013-10-03 10:35:53 -0400

    Collapse away test results when compilation fails
    Fixes #11.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -3,12 +3,12 @@ var config = {
     "reset.less", "scaffolding.less", "grid.less", "layouts.less",
     "type.less", "code.less", "labels-badges.less", "tables.less", "forms.less", "buttons.less",
     "button-groups.less", "navs.less", "navbar.less", "breadcrumbs.less", "alerts.less", "progress-bars.less",
-    "modals.less",
+    "modals.less", "accordion.less",
     "wells.less", "close.less", "utilities.less", "component-animations.less",
     "responsive-utilities.less", "responsive-767px-max.less", "responsive-768px-979px.less", "responsive-1200px-min.less", "responsive-navbar.less"
   ],
   js: [
-    "bootstrap-transition.js", "bootstrap-modal.js", "bootstrap-button.js"
+    "bootstrap-transition.js", "bootstrap-modal.js", "bootstrap-button.js", "bootstrap-collapse.js"
   ]
 };
 
